b89755c6fbdb4fcb18057f5c6bcde9de9ce021b9
zhangym <yongming9011@163.com>
2017-02-03 14:26:32 +0800

    the first commit after developed

---
+++ app/src/main/java/com/zhangym/downloaddemo/MainActivity.java
@@ -0,0 +1,73 @@
+package com.zhangym.downloaddemo;
+
+import android.Manifest;
+import android.app.DownloadManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.yanzhenjie.permission.AndPermission;
+import com.yanzhenjie.permission.PermissionListener;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_start_download).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (AndPermission.hasPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                    startDownload();
+                } else {
+                    AndPermission.with(MainActivity.this)
+                            .requestCode(100)
+                            .permission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                            .send();
+                }
+            }
+        });
+    }
+
+    private void startDownload() {
+        DownloadManager manager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse("http://apk.hiapk.com/appdown/com.zhangym.search_tools");
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        // 设置在下载过程中跟下载结束后都显示通知栏
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        // 设置在WIFI跟手机网络环境下下载
+        request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager.Request
+                .NETWORK_MOBILE);
+        // 设置下载位置为外部公共的下载目录，文件名为xiaoboshi.apk，并且可以被Media Scanner扫描到
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "xiaoboshi.apk");
+        // 设置允许系统扫描到下载的文件
+        request.allowScanningByMediaScanner();
+        long id = manager.enqueue(request);
+    }
+
+    private PermissionListener mPermissionListener = new PermissionListener() {
+        @Override
+        public void onSucceed(int requestCode, List<String> grantPermissions) {
+            startDownload();
+        }
+
+        @Override
+        public void onFailed(int requestCode, List<String> deniedPermissions) {
+            Toast.makeText(MainActivity.this, "未获取sd卡读写权限，无法下载！", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[]
+            grantResults) {
+        AndPermission.onRequestPermissionsResult(requestCode, permissions, grantResults, mPermissionListener);
+    }
+}
